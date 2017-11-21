8cb36fb0999730539e81dd78909c5ac18d60bf80
Joseph Ivie <josephivie@gmail.com>
2017-11-21 00:28:39 -0700

    NFC is working, auto-generated networking

--- src/main/java/com/lightningkite/kotlin/anko/viewcontrollers/implementations/VCActivity.kt
+++ src/main/java/com/lightningkite/kotlin/anko/viewcontrollers/implementations/VCActivity.kt
@@ -73,7 +73,6 @@ abstract class VCActivity : AppCompatActivity(), VCContext {
     override fun onNewIntent(intent: Intent) {
         super<AppCompatActivity>.onNewIntent(intent)
         onNewIntent.invokeAll(intent)
-        println("onNewIntent")
     }
 
     override fun onBackPressed() {
